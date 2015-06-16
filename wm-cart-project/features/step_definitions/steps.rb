a83ed6c89e6f0858b62d4550520339b87add610c
Leandro Mitsuo <leandromitsuooshiro@gmail.com>
2015-06-16 02:26:07 -0300

    commit inicial

---
+++ wm-cart-project/features/step_definitions/steps.rb
@@ -0,0 +1,40 @@
+Dado(/^que visito a pagina do walmart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado(/^busco pela palavra tv$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando(/^sistema exibe os resulados$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando(/^clico em um produto$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entao(/^sistema exibe os detalhes do produto$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entao(/^clico no botão adicionar ao carrinho$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entao(/^clico no radion button sem garantia estendida$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Entao(/^clino no botão continuar$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então(/^clico no botão do carrinho$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então(/^sistema exibe o produto selecionado$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
